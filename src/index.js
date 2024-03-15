a881130c520ad1b935715beb19a36974c65ed8c9
Shivam Singh <71212484+singhshivam7267@users.noreply.github.com>
2024-03-15 13:45:51 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Quiz from './components/Quiz'
 
-import './styles.css';
+import './styles.css'; 
 import { QuizData } from './components/QuizData';
 
 function App() {
